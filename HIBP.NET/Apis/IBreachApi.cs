c657cc1e51880f7525892de5c44bd6827cccd99d
Alex <alekcarlsen@gmail.com>
2018-02-04 20:21:51 +0100

    added sync versions to the interface as well

--- HIBP.NET/Apis/IBreachApi.cs
+++ HIBP.NET/Apis/IBreachApi.cs
@@ -6,6 +6,9 @@ namespace HIBP
 {
     public interface IBreachApi
     {
+        Breach GetBreach(string name);
+        IEnumerable<Breach> GetBreaches(string domain = null, bool includeUnverified = false);
+        IEnumerable<Breach> GetBreachesForAccount(string account, string domain = null, bool includeUnverified = false);
         Task<Breach> GetBreachAsync(string name);
         Task<IEnumerable<Breach>> GetBreachesAsync(string domain = null, bool includeUnverified = false);
         Task<IEnumerable<Breach>> GetBreachesForAccountAsync(string account, string domain = null, bool includeUnverified = false);
