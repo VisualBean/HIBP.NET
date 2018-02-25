86a2b54070953962b3c6ad3dbf1e9eb1473e82ed
Alex <alekcarlsen@gmail.com>
2018-02-25 12:19:02 +0100

    made breach "immutable"

--- HIBP.NET/Responses/Breach.cs
+++ HIBP.NET/Responses/Breach.cs
@@ -6,19 +6,50 @@ namespace HIBP.Responses
 {
     public class Breach
     {
-        public string Name { get; set; }
-        public string Title { get; set; }
-        public string Domain { get; set; }
-        public DateTime BreachDate { get; set; }
-        public DateTime AddedDate { get; set; }
-        public DateTime ModifiedDate { get; set; }
-        public int PwnCount { get; set; }
-        public string Description { get; set; }
-        public List<string> DataClasses { get; set; }
-        public bool IsVerified { get; set; }
-        public bool IsSensitive { get; set; }
-        public bool IsRetired { get; set; }
-        public bool IsSpamList { get; set; }
+        private readonly string _name;
+        private readonly string _title;
+        private readonly string _domain;
+        private readonly DateTime _breachDate;
+        private readonly DateTime _addedDate;
+        private readonly DateTime _modifiedDate;
+        private readonly int _pwnCount;
+        private readonly string _description;
+        private readonly List<string> _dataClasses;
+        private readonly bool _isVerified;
+        private readonly bool _isSensitive;
+        private readonly bool _isRetired;
+        private readonly bool _isSpamList;
+
+        public Breach(string name, string title, string domain, DateTime breachDate, DateTime addedDate, DateTime modifiedDate, int pwnCount, string description, List<string> dataClasses, bool isVerified, bool isSensitive, bool isRetired, bool isSpamList)
+        {
+            _name = name;
+            _title = title;
+            _domain = domain;
+            _breachDate = breachDate;
+            _addedDate = addedDate;
+            _modifiedDate = modifiedDate;
+            _pwnCount = pwnCount;
+            _description = description;
+            _dataClasses = dataClasses;
+            _isVerified = isVerified;
+            _isSensitive = isSensitive;
+            _isRetired = isRetired;
+            _isSpamList = isSpamList;
+        }
+
+        public string Name => _name;
+        public string Title => _title;
+        public string Domain => _domain;
+        public DateTime BreachDate => _breachDate;
+        public DateTime AddedDate => _addedDate;
+        public DateTime ModifiedDate => _modifiedDate;
+        public int PwnCount => _pwnCount;
+        public string Description => _description;
+        public List<string> DataClasses => _dataClasses;
+        public bool IsVerified => _isVerified;
+        public bool IsSensitive => _isSensitive;
+        public bool IsRetired => _isRetired;
+        public bool IsSpamList => _isSpamList;
 
         public override string ToString()
         {
@@ -32,7 +63,7 @@ namespace HIBP.Responses
             sb.AppendLine($"PwnCount: {PwnCount}");
             sb.AppendLine($"Description: {Description}");
             sb.AppendLine("DataClasses:");
-            foreach(var dataClass in DataClasses)
+            foreach (var dataClass in DataClasses)
             {
                 sb.AppendLine($"    {dataClass}");
             }
