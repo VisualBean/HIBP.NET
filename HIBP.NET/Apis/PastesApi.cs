eedb834484fac1a51e73c3cc328e6710e4b55227
Alex <alekcarlsen@gmail.com>
2020-12-07 20:30:11 +0100

    added docs to new API

--- HIBP.NET/Apis/PastesApi.cs
+++ HIBP.NET/Apis/PastesApi.cs
@@ -5,13 +5,31 @@
     using System.Threading.Tasks;
     using HIBP.Responses;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="HIBP.BaseApi" />
+    /// <seealso cref="HIBP.Apis.IPastesApi" />
     public class PastesApi : BaseApi, IPastesApi
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PastesApi"/> class.
+        /// </summary>
+        /// <param name="apiKey">The API key.</param>
+        /// <param name="serviceName">The service name.</param>
         public PastesApi(ApiKey apiKey, string serviceName)
             : base(apiKey, serviceName)
         {
         }
 
+        /// <summary>
+        /// Gets the pastes asynchronous.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>
+        /// A list of pastes.
+        /// </returns>
         public async Task<IEnumerable<Paste>> GetPastesAsync(Email email, CancellationToken cancellationToken = default)
         {
             return await this.GetAsync<IEnumerable<Paste>>($"pasteaccount/{email}", cancellationToken);
