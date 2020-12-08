af7da87e54c8374db472fb645446fca3dad83965
Alex <alekcarlsen@gmail.com>
2020-12-08 10:47:29 +0100

    rename parameter to reflect usage

--- HIBP.NET/Clients/IPwnedPasswordClient.cs
+++ HIBP.NET/Clients/IPwnedPasswordClient.cs
@@ -11,10 +11,10 @@
         /// <summary>
         /// Determines whether [is password pwned asynchronous] [the specified plain text password].
         /// </summary>
-        /// <param name="plainTextPassword">The plain text password.</param>
+        /// <param name="plainTextOrHashedPassword">The plain text or pre-hashed password.</param>
         /// <param name="isHash">if set to <c>true</c> [is hash].</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>The number of times a password has shown up as 'pwned'.</returns>
-        Task<int> IsPasswordPwnedAsync(string plainTextPassword, bool isHash = false, CancellationToken cancellationToken = default);
+        Task<int> IsPasswordPwnedAsync(string plainTextOrHashedPassword, bool isHash = false, CancellationToken cancellationToken = default);
     }
 }
